f7c0ca28b0dc4eb8a353ce4bee015ed730d27931
Tony Tong <liw.tong@timemicro.tech>
2025-03-05 17:28:17 +0800

    通过yq设置anchor alias

--- shared-env.sh
+++ shared-env.sh
@@ -4,7 +4,8 @@ src_yml=$1
 yml=$2
 env=${3:-"env"}
 
-echo "x-shared-env: &shared-env" > $yml
+echo "" > $yml
+yq eval ".x-shared-env anchor=\"shared-env\"" $yml -i
 while IFS= read -r line; do
     if [[ "$line" =~ ^([^=]+)=(.*)$ ]]; then
         key="${BASH_REMATCH[1]}"
@@ -16,13 +17,11 @@ while IFS= read -r line; do
 done < <(grep -vE "^(#.*|\s*)$" "$env") 
 
 for item1 in $(yq eval ". | to_entries[] | .key" $src_yml); do
-    if [ $item1 == "services" ]; then
-        yq eval ".$item1 = {}" $yml -i    
+    if [ $item1 == "services" ]; then 
         for item2 in $(yq eval ".$item1 | to_entries[] | .key" $src_yml); do
-            yq eval ".$item1.$item2 = {}" $yml -i  
             for item3 in $(yq eval ".$item1.$item2 | to_entries[] | .key" $src_yml); do              
                 if [ $item3 == "env_file" ] || [ $item3 == "environment" ]; then
-                    yq eval ".$item1.$item2.environment = {\"<<\":\"*shared-env\"}" $yml -i
+                    yq eval ".$item1.$item2.environment.<< alias= \"shared-env\"" $yml -i
                     if [ $(yq eval ".$item1.$item2 | has(\"environment\")" $src_yml) == true ]; then
                         if [ $(yq eval ".$item1.$item2.environment  | type" $src_yml) == "!!seq" ]; then
                             for kvs in $(yq eval ".$item1.$item2.environment[]" $src_yml); do
@@ -46,4 +45,4 @@ for item1 in $(yq eval ". | to_entries[] | .key" $src_yml); do
     fi    
 done
 
-sed -i "s/!!merge <<: '\*shared-env'/<<: *shared-env/g" $yml
+yq eval "(... | select(tag == \"!!merge\")) tag = \"\"" $yml -i
